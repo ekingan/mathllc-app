32dce94811fa13eaf8e412a8d2b8f4017ad16420
ekingan <ekingan@gmail.com>
2017-06-16 23:08:18 -0700

    precompiled assets

--- app/admin/wage.rb
+++ app/admin/wage.rb
@@ -1,15 +1,45 @@
 ActiveAdmin.register Wage do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+	belongs_to :preparer
+	permit_params :preparer_attributes, :amount
+
+	filter :preparer
+	filter :amount
+	filter :created_at
+
+	index do
+    column :id
+    column "Preparer" do |wage|
+      link_to Preparer.find(wage.preparer_id), admin_preparer_path(wage.preparer_id)
+    end
+    column :job_id
+    column "Amount" do |wage|
+    	number_to_currency(wage.amount)
+    end
+    column :created_at
+    actions
+  end
+
+	show do
+		attributes_table do
+			row :job_id
+			row "Preparer" do
+				link_to "#{wage.preparer.first_name}  #{wage.preparer.last_name}", admin_preparer_path(wage.preparer)
+			end
+			row "Amount" do
+				number_to_currency(payment.amount)
+			end
+			row "Received On" do
+				payment.created_at
+			end
+		end
+	end
+
+	form do |f|
+    f.semantic_errors *f.object.errors.keys
+		f.inputs "Wage Info" do
+			f.input :amount
+		end
+		f.actions
+	end
 
 end
