87dd5c6a2204d85a9aad1dbb7d3af8efe0aee5cc
ekingan <ekingan@gmail.com>
2017-07-04 14:06:51 -0700

    resolving migration issue on type

--- db/migrate/20170618161221_add_type_to_job.rb
+++ db/migrate/20170618161221_add_type_to_job.rb
@@ -1,10 +1,10 @@
 class AddTypeToJob < ActiveRecord::Migration[5.1]
   def up
-    add_column :jobs, :job_type, :integer
+    add_column :jobs, :job, :integer
   end
 
   def down
-    remove_column :jobs, :job_type
+    remove_column :jobs, :job
   end
 
 end
