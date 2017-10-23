aa5046a0eab01c995567cbbce1963f037ca5211d
ekingan <ekingan@gmail.com>
2017-10-23 15:52:50 -0700

    Include only ready jobs in unpaid

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -58,7 +58,7 @@ ActiveAdmin.register_page "Dashboard" do
       column do
         panel "Unpaid Jobs" do
           ol do
-            jobs.each do |job|
+            jobs.where(status: :filed).each do |job|
               li link_to("#{job.client.name} - #{job.status}", admin_job_path(job)) unless job.payment
             end
           end
@@ -70,7 +70,7 @@ ActiveAdmin.register_page "Dashboard" do
         panel "Filed Jobs" do
           ol do
             jobs.where(status: :filed).map do |job|
-              li link_to("#{job.client.name} - #{job.status}", admin_job_path(job))
+              li link_to("#{job.client.name} - #{job.bill}", admin_job_path(job))
             end
           end
         end
