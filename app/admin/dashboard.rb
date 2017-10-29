6cc7d7dc0ea526c5d99600e496effc672199bf74
ekingan <ekingan@gmail.com>
2017-10-29 13:56:39 -0700

    Ordering

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -79,7 +79,7 @@ ActiveAdmin.register_page "Dashboard" do
         panel "Recent Payments" do
           ol do
             #refactor this
-            jobs.paid..limit(35).each do |job|
+            jobs.paid.limit(35).each do |job|
               Payment.where(job_id: job.id).map do |pay|
                 li link_to("#{number_to_currency(pay.amount)} - #{pay.job.client.name} - Received on  #{pay.created_at.to_date.strftime("%m/%d/%Y")} ", admin_payment_path(pay))
               end
