aaec67a146f2d8470ddbcf7b59a2b28cbed5372b
ekingan <ekingan@gmail.com>
2018-09-14 10:21:40 -0700

    WIP

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,7 @@
 class Job < ApplicationRecord
   belongs_to :client
   belongs_to :preparer
-  has_one :payment
+  has_many :payments
   enum fed_form: [:Individual_1040, :S_Corp_1120S, :Partnership_1065, :C_Corp_1120, :Non_Profit_990, :Trust_1041, :Estate_706, :Amendment, :other]
   enum status: [:commited, :todo, :in_progress, :need_info, :need_signatures, :ready, :filed, :done, :extended, :accepted, :rejected, :review]
   enum job_type: [:bookkeeping, :consulting, :referral, :teaching]
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,8 +1,17 @@
 class Payment < ApplicationRecord
   belongs_to :job
   enum payment_type: [:check, :cash, :square, :paypal, :qbo, :stripe, :trade, :other]
-  validates_presence_of :amount
-  validates :amount, numericality: {
-    equal_to: ->(payment) { payment.job.bill }
-  }
+  validates_presence_of :amount, numericality: true
+  validate :payment_amount, :partial_payment_amount
+
+  def payment_amount
+    errors.add(:amount, "The payment amount must equal the bill amount #{job.bill}") unless amount = job.bill
+  end
+
+  def partial_payment_amount
+    return true unless partial_payment
+    payments = job.payments.sum(&:amount)
+    errors.add(:amount, "The payment amount must be less than or equal to the bill amount") unless (payments + amount <= job.bill)
+  end
+
 end
