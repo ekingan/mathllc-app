203f766970ab72389cf546a88cf0890fe6be6da9
ekingan <ekingan@gmail.com>
2018-09-14 12:29:54 -0700

    WIP

--- app/models/job.rb
+++ app/models/job.rb
@@ -20,6 +20,8 @@ class Job < ApplicationRecord
 
   scope :billed, -> { where('bill!=?', 0.0) }
 
+  scope :balance_due, -> { select('sum(payments.amount) as total_payments, jobs.id').joins('left outer join payments on payments.job_id = jobs.id').group('jobs.id').having('sum(payments.amount) < jobs.bill') }
+
   def to_param
     "#{id} #{client.last_name}".parameterize
   end
@@ -44,4 +46,9 @@ class Job < ApplicationRecord
   def set_to_accepted_status
     self.update_attribute(:status, :accepted)
   end
+
+  def amount_due
+    bill - payments.sum(&:amount)
+  end
+
 end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,16 +2,16 @@ class Payment < ApplicationRecord
   belongs_to :job
   enum payment_type: [:check, :cash, :square, :paypal, :qbo, :stripe, :trade, :other]
   validates_presence_of :amount, numericality: true
-  validate :payment_amount, :partial_payment_amount
+  validate :payment_amount
 
   def payment_amount
-    errors.add(:amount, "The payment amount must equal the bill amount #{job.bill}") unless amount = job.bill
-  end
-
-  def partial_payment_amount
-    return true unless partial_payment
     payments = job.payments.sum(&:amount)
-    errors.add(:amount, "The payment amount must be less than or equal to the bill amount") unless (payments + amount <= job.bill)
+    total_due = job.bill
+    if partial_payment
+      errors.add(:payment, "This payment must not exceed $#{job.bill - payments}") unless (payments + amount <= total_due)
+    else
+      errors.add(:payment, "This payment must equal $#{total_due}. If this is a partial payment, please check the partial payment box") unless (amount == total_due)
+    end
   end
 
 end
